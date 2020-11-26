1163c59b9a6619d26a258bd72fa209d96b484050
andergoncalves <andergoncalves@gmail.com>
2020-11-26 15:11:03 -0300

    categoria

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 require_once("functions.php");
 
 use Hcode\Model\User;
+use Hcode\Model\Category;
 use Hcode\Page;
 use Hcode\PageAdmin;
 
@@ -25,7 +26,7 @@ $app->get('/admin', function() {
 
 	$page = new PageAdmin();
 
-	$page->setTpl("login");
+	$page->setTpl("index");
 
 });
 
@@ -220,6 +221,86 @@ $app->post("/admin/forgot/reset", function(){
 	]);
 
 	$page->setTpl("forgot-reset-success");
+
+});
+
+$app->get("/admin/categories", function(){
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		'categories'=>$categories
+	]);
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
+});
+
+$app->get('/admin/categories/:idcategory/delete', function($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get('/admin/categories/:idcategory', function($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update",[
+		'category'=>$category->getValues()
+	]);
+
+});
+
+$app->post('/admin/categories/:idcategory', function($idcategory) {
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
 
 });
 
